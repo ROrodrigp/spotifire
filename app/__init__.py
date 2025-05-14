fb72b48bfdce3dd7ecaaae25a442719144c927aa
ROrodrigp <rodrigo.agp96@gmail.com>
2025-05-13 22:17:13 -0600

    Adding CustomFileSystemSessionInterface

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask.sessions import FileSystemSessionInterface
+import os
 import logging
 import sys
 from config import Config
@@ -13,9 +15,22 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+class CustomFileSystemSessionInterface(FileSystemSessionInterface):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.use_signer = True
+
 def create_app(config_class=Config):
     app = Flask(__name__, template_folder='../templates', static_folder='../static')
     app.config.from_object(config_class)
+    
+    # Configurar sesiones basadas en archivos
+    session_dir = os.path.join(config_class.APP_DIR, "data", "sessions")
+    os.makedirs(session_dir, exist_ok=True)
+    app.session_interface = CustomFileSystemSessionInterface(
+        session_dir, 
+        app.secret_key
+    )
     
     # Asegurar que existen los directorios necesarios
     Config.init_app()
